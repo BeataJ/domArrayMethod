4f2643044b11c7eb88c325d89cfd708459367b5a
beata <beata@emc2wave.com>
2020-05-04 17:50:51 -0700

    initialize data array

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const main = document.getElementById('double');
 const main = document.getElementById('show-millionaires');
 const main = document.getElementById('sort');
 const main = document.getElementById('calculate-wealth');
+
+let data = [];
